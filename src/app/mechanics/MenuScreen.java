62b123b173ff3c718d8ee93c3bc25b4a426c7030
Oliver <mail2oliverb@gmail.com>
2023-08-27 15:57:17 +0200

    Updated menu screen

--- src/app/mechanics/MenuScreen.java
+++ src/app/mechanics/MenuScreen.java
@@ -1,7 +1,12 @@
 package app.mechanics;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Logger;
 
 /**
@@ -14,6 +19,7 @@ public class MenuScreen extends JPanel {
     private Dimension preferredSize;
     private JButton startButton;
     private JButton exitButton;
+    private BufferedImage backgroundImage = null;
     private static final Logger LOGGER = Logger.getLogger("GameLogger");
 
     /**
@@ -23,15 +29,42 @@ public class MenuScreen extends JPanel {
      */
     public MenuScreen(Dimension compSize) {
 	this.preferredSize = compSize;
+
 	setPreferredSize(preferredSize);
-	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	setLayout(new GridBagLayout());
+
+	GridBagConstraints constraints = new GridBagConstraints();
+	constraints.anchor = GridBagConstraints.CENTER;
+	constraints.gridwidth = GridBagConstraints.REMAINDER;
+	constraints.insets = new Insets(10, 0, 10, 0);
+
 
 	this.startButton = new JButton("Start Game");
+
 	this.exitButton = new JButton("Exit Game");
 
-	add(startButton);
-	add(exitButton);
+	initButtonsStyle(new JButton[] { startButton, exitButton });
+
+	JPanel buttons = new JPanel(new GridBagLayout());
+	buttons.add(startButton, constraints);
+	buttons.add(exitButton, constraints);
+	buttons.setOpaque(false);
+
+	add(buttons, constraints);
+
+	try {
+	    URL url = ClassLoader.getSystemResource("images/block.jpg");
+	    this.backgroundImage = ImageIO.read(url);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
 	LOGGER.config("Menuscreen successfully initialized");
+    }
+
+    @Override protected void paintComponent(final Graphics g) {
+	super.paintComponent(g);
+	g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
     }
 
     @Override public Dimension getPreferredSize() {
@@ -44,5 +77,13 @@ public class MenuScreen extends JPanel {
 
     public JButton getExitButton() {
 	return exitButton;
+    }
+
+    private void initButtonsStyle(JButton[] buttons) {
+	for (JButton button : buttons) {
+	    button.setPreferredSize(new Dimension(preferredSize.width/4, preferredSize.height/12));
+	    button.setBackground(Color.GRAY);
+	    button.setForeground(Color.WHITE);
+	}
     }
 }
